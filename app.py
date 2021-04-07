932874c908959ce31c15ac2cad3ccb64ac826709
James Mortifee <james.mortifee@hotmail.co.uk>
2021-04-07 17:46:14 +0100

    logout functionality

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def landing():
 @app.route("/recipes")
 def recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("landing.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes)
 
 
 @app.route("/sign_up", methods=["GET", "POST"])
@@ -88,7 +88,18 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
